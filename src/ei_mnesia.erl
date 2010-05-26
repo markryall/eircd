e9ae9037584e46e8fef69b73552148ad401ca98e
tom <tom@tomwilson.name>
2010-05-26 12:02:18 +1000

    updated .gitignore. added start of mnesia integration

---
+++ src/ei_mnesia.erl
@@ -0,0 +1,28 @@
+-module(ei_mnesia).
+
+-include_lib("stdlib/include/qlc.hrl").
+
+-export([init/0, insert/2, select/1]).
+
+-record(user, {nick, pid}).
+
+init() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+    mnesia:create_table(user,
+			 [{attributes, record_info(fields, user)}]).
+
+insert(Nick, Pid) ->
+    Fun = 
+	fun() ->
+		mnesia:write(#user{nick=Nick, pid=Pid})
+	end,
+    mnesia:transaction(Fun).
+
+select(Pid) ->
+    Fun = 
+	fun() ->
+		User = #user{pid = Pid, nick = '$1', _ = '_'},
+		mnesia:select(user, [{User, [], ['$1']}])
+	end,
+    mnesia:transaction(Fun).
