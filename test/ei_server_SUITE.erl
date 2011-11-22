a6b3593890eff9130b4b22e51a165a76fb948814
Tom Wilson <tom@tomwilson.name>
2011-11-23 08:35:30 +1000

    changed cucumber user registration to common test

--- test/ei_server_SUITE.erl
+++ test/ei_server_SUITE.erl
@@ -6,7 +6,7 @@
 
 -define(HOST, "127.0.0.1").
 -define(PORT, 6667).
--define(TCP_OPTIONS, [{packet, 0}]).
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}]).
 
 all() -> [test_registration].
 
@@ -25,9 +25,9 @@ end_per_testcase(TestCase, Config) ->
   Config.
 
 test_registration(Config) ->
-  State = {},
   {ok, Sock} = gen_tcp:connect(?HOST, ?PORT, ?TCP_OPTIONS),
-  ok = gen_tcp:send(Sock, "NICK abc\r\n"),
+  ok = gen_tcp:send(Sock, "NICK tom\r\n"),
   ok = gen_tcp:send(Sock, "USER a b c d e\r\n"),
+  {ok, <<":eircd 001 tom :Welcome to the eircd Internet Relay Chat Network tom\r\n">>} = gen_tcp:recv(Sock, 0),
   ok = gen_tcp:close(Sock).
 
