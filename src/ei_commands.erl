a8778ca1317cbe6e1016e43aa81cf704e54bad0e
Tom Wilson <tom@tomwilson.name>
2011-11-24 09:40:39 +1000

    no more apply

--- src/ei_commands.erl
+++ src/ei_commands.erl
@@ -31,24 +31,23 @@ process_command(Command, State) ->
     {ok, State}.
 
 user(Pid, Arguments, State) ->
-    %?LOG(io_lib:format("processing user command with args ~p", [Arguments])),
     [Username, Hostname, Servername|_Realname] = Arguments,
 
     % TODO: replace second Username below with Realname
-    apply(ei_mod_userinfo, handle_event, [{user_userinfo_registration, {Pid, Username, Hostname, Servername, Username}}, State]).
+    ei_mod_userinfo:handle_event({user_userinfo_registration, {Pid, Username, Hostname, Servername, Username}}, State).
 
 nick(Nick, State) ->
-    apply(ei_mod_nick, handle_event, [{user_nick_registration, {Nick}}, State]).
+    ei_mod_nick:handle_event({user_nick_registration, {Nick}}, State).
 
 ping(Pid, _, State) ->
-    apply(ei_mod_ping, handle_event, [{user_ping, {Pid}}, State]).
+    ei_mod_ping:handle_event({user_ping, {Pid}}, State).
 
 join(Pid, Channel, State) ->
-    apply(ei_mod_join, handle_event, [{user_join, {Pid, Channel}}, State]).
+    ei_mod_join:handle_event({user_join, {Pid, Channel}}, State).
     
 part(Pid, Channel, State) ->
-    apply(ei_mod_part, handle_event, [{user_part, {Pid, Channel}}, State]).
+    ei_mod_part:handle_event({user_part, {Pid, Channel}}, State).
 
 privmsg(Pid, Args, State) ->
     [Channel|Msg] = Args,
-    apply(ei_mod_privmsg, handle_event, [{user_privmsg, {Pid, Channel, Msg}}, State]).
+    ei_mod_privmsg:handle_event({user_privmsg, {Pid, Channel, Msg}}, State).
