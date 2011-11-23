8c4744089cade34a3807b4ecc279acf065108c46
Tom Wilson <tom@tomwilson.name>
2011-11-23 14:22:33 +1000

    cleaned up ei_mod_nick.erl

--- src/ei_mod_nick.erl
+++ src/ei_mod_nick.erl
@@ -1,36 +1,10 @@
 -module(ei_mod_nick).
 
--behaviour(gen_event).
-
 -export([
-	 init/1
-	 ]).
--export([
-	 handle_event/2,
-	 handle_call/2,
-	 handle_info/2,
-	 code_change/3,
-	 terminate/2
+	 handle_event/2
 	 ]).
 
 -include_lib("ei_logging.hrl").
-
--record(state, {}).
-
-init([]) ->
-    {ok, #state{}}.
-
-terminate(_Reason, State) ->
-    {noreply, State}.
-
-handle_call(Msg, State) ->
-    {reply, {ok, Msg}, State}.
-
-handle_info(_, State) ->
-    {noreply, State}.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
 
 handle_event({user_nick_registration, {Pid, Nick}}, State) ->
     ?LOG(io_lib:format("processing nick registration event with nick: ~p", [Nick])),
